063cc85fa3c1060721f89b7e95d7c69cdb8d6e36
John Horback <jhorback@gmail.com>
2021-07-17 11:43:19 -0400

    Converted linkProp to ts and started on tests

--- packages/linkProp/linkProp.spec.ts
+++ packages/linkProp/linkProp.spec.ts
@@ -1,11 +1,27 @@
 import { describe, it, expect } from "@jest/globals";
+import {LitElement, property, customElement, html} from "lit-element";
+
+
 import {
     linkProp,
-    linkChecked,
     linkSiblingProp,
     linkVal,
+    linkChecked,
     setPropertyPath
-  } from "./linkProp";
+} from "./linkProp";
+
+@customElement("test-element")
+class TestElement extends LitElement {
+    
+    @property({ type: String})
+    state = "initial state";
+
+    render() {
+        return html`
+            <div id="testLinkProp" @state-changed="${linkProp(this, "state")}"></div>
+        `;
+    }
+}
 
 
 describe("linkProp", () => {
@@ -17,5 +33,18 @@ describe("linkProp", () => {
         expect(linkVal).not.toBeNull();
         expect(setPropertyPath).not.toBeNull();
     });
-    
+
+    it("does something", () => {
+        // const testElement = new TestElement();
+        const testElement = document.createElement("test-element") as TestElement;
+        document.body.appendChild(testElement);
+        const testDiv = testElement.shadowRoot?.querySelector("#testLinkProp")
+        console.log("TESTDIV", document.body.innerHTML);
+        //@ts-ignore
+        testDiv.state = "updated state!";
+        expect(testElement.state).toEqual("initial state");
+        testDiv?.dispatchEvent(new CustomEvent("state-changed"));
+        expect(testElement.state).toEqual("updated state!");
+
+    })
 });
