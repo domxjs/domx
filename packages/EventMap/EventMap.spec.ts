ee8d0e5f0544839a428eb7881ef2ea6340ab516a
John Horback <jhorback@gmail.com>
2021-07-22 23:21:32 -0400

    Updated linkProp structure

--- packages/EventMap/EventMap.spec.ts
+++ packages/EventMap/EventMap.spec.ts
@@ -1,7 +1,7 @@
 import { describe, test, expect } from "@jest/globals";
 import { EventMap } from "./EventMap";
 
-describe("EventMap", () => {
+describe.skip("EventMap", () => {
 
     test("it is true", () => {
         expect(true).toBe(true);
