b31577a76ea155e8ef2092a99239edca93ef8542
John Horback <jhorback@gmail.com>
2021-08-11 23:09:24 -0400

    Ignored rdt build files

--- packages/StateChange/applyStateChangeRdtLogging.d.ts
+++
@@ -1,9 +0,0 @@
-export { applyStateChangeRdtLogging };
-/**
- * Redux Dev Tools Middleware
- *
- * Docs:
- * https://github.com/zalmoxisus/redux-devtools-extension/blob/master/docs/API/Methods.md
- */
-declare const applyStateChangeRdtLogging: () => void;
-//# sourceMappingURL=applyStateChangeRdtLogging.d.ts.map
