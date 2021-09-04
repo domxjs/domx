cbc9d41ce55c6a799d9eb9b389e5447ab4a0dbea
John Horback <jhorback@gmail.com>
2021-09-04 15:22:01 -0400

    #18 - implemented fixture and tests

--- packages/testUtils/src/fixture.ts
+++ packages/testUtils/src/fixture.ts
@@ -1,4 +1,18 @@
-import {html} from "lit-html";
+import {html, render, TemplateResult} from "lit-html";
 export { fixture, html};
 
-const fixture = () => {};
+interface FixtureElement extends HTMLElement {
+    restore: Function
+};
+  
+function fixture<T>(html:TemplateResult): FixtureElement & T {
+  const fixture = document.createElement("div");
+  fixture.setAttribute("fixture", "");
+  document.body.appendChild(fixture);
+
+  render(html, fixture);
+  const el = fixture.firstElementChild as FixtureElement & T;
+
+  el.restore = () => fixture.remove()
+  return el;
+};
