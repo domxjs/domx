12ce7910681607126e03ceaabce716012be357aa
John Horback <jhorback@gmail.com>
2021-09-05 14:20:07 -0400

    Expanded dataProperties for FR - DataElement #13

--- packages/DataElement/src/DataElement.ts
+++ packages/DataElement/src/DataElement.ts
@@ -1,16 +1,62 @@
 export {
     customDataElements,
     customDataElement,
-    DataElement
+    DataElement,
+    DataElementCtor
 };
 
+declare global {
+    interface HTMLElement {
+        connectedCallback(): void;
+        disconnectedCallback(): void;
+    }
+}
 
-class DataElement extends HTMLElement {
+/**
+ * Defines the static fields of 
+ * a DataElement
+ */
+interface DataElementCtor extends Function {
+    __elementName: string,
+    dataProperties: DataProperties;
+    statIdProperty: string|null
+}
+
+interface DataProperties {
+    [key:string]: DataProperty
+}
+
+interface DataProperty {
+    changeEvent?: string
+    statePath?: string
 }
 
 
+class DataElement extends HTMLElement {
+    static __elementNaem = "";
+    static stateIdProperty:string|null = null;
+    static dataProperties:DataProperties = {
+        state: {changeEvent:"state-changed"}
+    };
 
+    connectedCallback() {
+        super.connectedCallback && super.connectedCallback();
 
+        setUpDataProperties(this);
+
+        // keep track of who is using state for each state path
+        //->stateRefs[statePath] = stateRefs[statePath] ? stateRefs[statePath]+1 : 0;
+        // decrease the ref count on disconnect and if 0 then delete the item from RootState
+
+         // for each dataProperty set the initial state
+         //->RootState.get(this)
+         // if this is null then use the state property
+         // on this element and set it in RootState
+         // would need to do this for every data property.
+
+         // Hook up global and local events (see below)
+    }
+}
 
 /**
  * Custom DataElement Registry
@@ -29,7 +75,6 @@ const customDataElements = {
     }
 };
 
-
 /**
  * Defines the custom element with window.customElements.define
  * and tags the element name for use in RootState.
@@ -38,3 +83,24 @@ const customDataElements = {
 const customDataElement = (elementName:string) =>
     (element: CustomElementConstructor) =>
         customDataElements.define(elementName, element);
+
+
+const setUpDataProperties = (el:DataElement) => {
+    const ctor = el.constructor as DataElementCtor;
+        
+    //@ts-ignore TS7053 property index
+    const stateId = ctor.stateIdProperty && el[ctor.stateIdProperty];
+
+    const stateIdPath = stateId ? `.${stateId}` : "";
+    Object.keys(ctor.dataProperties).map((propertyName) => {
+        const dp = ctor.dataProperties[propertyName];
+        ctor.dataProperties[propertyName] = {
+            ...dp,
+            changeEvent: dp.changeEvent || `${propertyName}-changed`,
+            statePath: `${ctor.__elementName}.${propertyName}${stateIdPath}`
+        }
+    });
+
+    // pass to middleware here
+    // Middleware will have the dataProperties, elementName, and element
+};
--- packages/DataElement/src/test/DataElement.spec.ts
+++ packages/DataElement/src/test/DataElement.spec.ts
@@ -2,25 +2,51 @@ import { describe, it, expect } from "@jest/globals";
 import { fixture, html} from "@domx/testutils/fixture";
 import {
     customDataElement,
-    DataElement
+    DataElement,
+    DataElementCtor
 } from "../DataElement";
 
 
 describe("DataElement", () => {
     it("it can define an element", () => {
-        const el = fixture(html`<test-data-element></test-data-element>`)
+        const el = fixture(html`<test-data-element></test-data-element>`);
         expect(el.tagName).toBe("TEST-DATA-ELEMENT");
     });
 
     it("it adds the __elementName", () => {
-        const el = fixture(html`<test-data-element></test-data-element>`)
-        //@ts-ignore TS2339 checking dynamic property
-        expect(el.constructor.__elementName).toBe("test-data-element");
+        const el = fixture(html`<test-data-element></test-data-element>`);
+        expect((el.constructor as DataElementCtor).__elementName).toBe("test-data-element");
+    });
+
+    it("expands the data properties", () => {
+        const el = fixture(html`<test-data-element></test-data-element>`);
+        const ctor = el.constructor as DataElementCtor;
+        expect(ctor.dataProperties["state"].statePath).toBe("test-data-element.state");
+    });
+
+    it("expands the data properties with a stateId", () => {
+        const el = fixture(html`<test-instance-data-element user-id="1234"></test-instance-data-element>`);
+        const ctor = el.constructor as DataElementCtor;
+        expect(ctor.dataProperties["user"].statePath).toBe("test-instance-data-element.user.1234");
     });
 });
 
 
 @customDataElement("test-data-element")
 class TestDataElement extends DataElement {
+}
 
+@customDataElement("test-instance-data-element")
+class TestInstanceDataElement extends DataElement {
+    static stateIdProperty = "userId";
+    static dataProperties = {
+        "user": {changeEvent:"user-changed"}
+    }
+
+    userId:string|null = null;
+
+    constructor() {
+        super();
+        this.userId = this.getAttribute("user-id");    
+    }
 }
